5421a1e57ea5f6c91a6c4a9618b5c2788c9167e7
eliahau <eliahau@stud.ntnu.no>
2025-10-21 21:45:08 +0200

    Started adding the JSON reading and writing.

--- src/main/java/edu/ntnu/iir/bidata/Main.java
+++ src/main/java/edu/ntnu/iir/bidata/Main.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        //Checking if data.json is present, if not, it is created.
         File file = new File("database/data.json");
         if (!file.exists()) {
             System.out.println("No database file found, starting fresh.");
@@ -20,14 +21,15 @@ public class Main {
                 e.printStackTrace();
             }
         }
-
+        // readerWriter to handle Output and Intput to and from the JSON file
         JsonReaderWriter readerWriter = new JsonReaderWriter("database/data.json");
 
+        // Initiating a list with all the diferent diary entries
         List<DiaryEntry> diaryEntries = readerWriter.readObjects();
         if (diaryEntries == null) {
             diaryEntries = new ArrayList<>();
         }
-
+        // The menu loop is started
         Menu menu = new Menu();
         menu.menu();
         diaryEntries.add(new DiaryEntry());
