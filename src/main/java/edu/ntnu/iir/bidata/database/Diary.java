67c696d5497c213a82b6e116d71a9776e734119a
Elias Haugsbakk <elias.haugsbakk@gmail.com>
2025-11-10 15:28:47 +0100

    Diary.java to hold the DiaryEntry instantiates.

--- src/main/java/edu/ntnu/iir/bidata/database/Diary.java
+++ src/main/java/edu/ntnu/iir/bidata/database/Diary.java
@@ -1,4 +1,34 @@
 package edu.ntnu.iir.bidata.database;
 
+import edu.ntnu.iir.bidata.utils.DiaryEntry;
+import java.util.ArrayList;
+
+/**
+ * DiaryDatabase class to store the Array with diary objects. Array with author variables. Array
+ * with categories variable.
+ */
 public class Diary {
+  private ArrayList<DiaryEntry> entries = new ArrayList<>();
+
+  public void addEntry(DiaryEntry diaryEntry) {
+    entries.add(diaryEntry);
+  }
+
+  public DiaryEntry getEntry(int index) {
+    if (index >= 0 || index <= entries.size()) {
+      return entries.get(index);
+    } else {
+      return null;
+    }
+  }
+
+  /**
+   * Geetter to return the ArrayList of diayr entries. Return copy for safety.
+   *
+   * @return
+   */
+  public ArrayList<DiaryEntry> getEntries() {
+    ArrayList<DiaryEntry> entriesCopy = new ArrayList<>(entries);
+    return entriesCopy;
+  }
 }
